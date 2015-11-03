29ff2ae988c6b0beaf67681b7364a151d4ec8051
Stephan Wallentin <stephan.wallentin@gmail.com>
2015-11-03 23:09:12 +0100

    Added mongodb integration against players and matches collections. Changed overall fetching mechanics for data, lists of id's are now stored in redis and used as mongodb queries. Added error handling. All routes now enjoy complete json responses

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 var config = require('config'),
     apiConfig = config.get('api'),
     redis = require('redis'),
+    Server = require('./lib/server'),
     redisClient = redis.createClient(),
-    server = require('./lib/server')(redisClient, apiConfig);
+    MongoClient = require('mongodb').MongoClient,
+    url = 'mongodb://localhost:27017/ql-stats';
 
-server.listen(apiConfig.server.port, apiConfig.server.host, function (err) {
-    if (err) {
-        return console.log(err);
-    }
-    console.log('%s listening at %s', server.name, server.url);
+redisClient.on('ready', function() {
+
+    MongoClient.connect(url, function (err, db) {
+        var server = Server.create(redisClient, db, apiConfig);
+
+        server.listen(apiConfig.server.port, apiConfig.server.host, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log('%s listening at %s', server.name, server.url);
+        });
+
+    });
+
 });
+
+
